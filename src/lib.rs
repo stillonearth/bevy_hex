3a004ad623e04ac391d9144f23ca31a75d1c9ae0
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2022-10-07 08:38:44 +0300

    upd lib

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use bevy::{
+    prelude::*,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+};
+
+pub mod geometry;
+pub mod hex;
+
+/// Generate a single hex mesh
+pub fn generate_hex_mesh() -> Mesh {
+    let mut pts: Vec<[f32; 3]> = vec![];
+    let c = hex::HexCoord::new(0, 0);
+    geometry::bevel_hexagon_points(&mut pts, 1.0, 0.95, &c);
+
+    let mut normals: Vec<[f32; 3]> = vec![];
+    geometry::bevel_hexagon_normals(&mut normals);
+
+    let mut uvs: Vec<[f32; 2]> = vec![];
+    for _ in 0..pts.len() {
+        uvs.push([0., 0.]);
+    }
+
+    let mut indices = vec![];
+    geometry::bevel_hexagon_indices(&mut indices);
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.set_indices(Some(Indices::U32(indices)));
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, pts);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    mesh
+}
